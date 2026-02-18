4cce0b9da4443fa297cec8041b17d778e6870f10
sebasrd208 <sebaslascurain@gmail.com>
2026-02-17 20:05:26 -0600

    Gestion de recibos nomina y envio de correo

---
+++ MyBatis/src/main/java/com/example/mybatis/DTO/ResultadoEnvioDTO.java
@@ -0,0 +1,66 @@
+package com.example.mybatis.DTO;
+
+import java.util.List;
+
+/**
+ *
+ * @author sebas
+ */
+public class ResultadoEnvioDTO {
+    private List<String> correosEnviados;
+    private List<String> correosFallidos;
+    private int totalEnviados;
+    private int totalFallidos;
+    private long tiempoEjecucionMs;
+
+    public ResultadoEnvioDTO() {
+    }
+
+    public ResultadoEnvioDTO(List<String> correosEnviados, List<String> correosFallidos, int totalEnviados, int totalFallidos, long tiempoEjecucionMs) {
+        this.correosEnviados = correosEnviados;
+        this.correosFallidos = correosFallidos;
+        this.totalEnviados = totalEnviados;
+        this.totalFallidos = totalFallidos;
+        this.tiempoEjecucionMs = tiempoEjecucionMs;
+    }
+
+    public List<String> getCorreosEnviados() {
+        return correosEnviados;
+    }
+
+    public void setCorreosEnviados(List<String> correosEnviados) {
+        this.correosEnviados = correosEnviados;
+    }
+
+    public List<String> getCorreosFallidos() {
+        return correosFallidos;
+    }
+
+    public void setCorreosFallidos(List<String> correosFallidos) {
+        this.correosFallidos = correosFallidos;
+    }
+
+    public int getTotalEnviados() {
+        return totalEnviados;
+    }
+
+    public void setTotalEnviados(int totalEnviados) {
+        this.totalEnviados = totalEnviados;
+    }
+
+    public int getTotalFallidos() {
+        return totalFallidos;
+    }
+
+    public void setTotalFallidos(int totalFallidos) {
+        this.totalFallidos = totalFallidos;
+    }
+
+    public long getTiempoEjecucionMs() {
+        return tiempoEjecucionMs;
+    }
+
+    public void setTiempoEjecucionMs(long tiempoEjecucionMs) {
+        this.tiempoEjecucionMs = tiempoEjecucionMs;
+    }           
+}
